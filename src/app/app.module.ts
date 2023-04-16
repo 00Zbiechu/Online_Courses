dcce5441572eaae12e18240491db2eea08e7b859
Windows <00Zbiewski@gmail.com>
2023-04-16 11:02:08 +0200

    Rozpoczęcie pracy nad szablonem strony logowania

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { CourseListComponent } from './course-list/course-list.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CourseListComponent } from './course-list/course-list.component';
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
-    CourseListComponent
+    CourseListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
