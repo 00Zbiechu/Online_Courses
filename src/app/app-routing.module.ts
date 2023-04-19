60260c187b41f6ad904eed5eb0f4b0a1d1bc5714
Windows <00Zbiewski@gmail.com>
2023-04-19 14:55:01 +0200

    Zmiana struktury - dodanie modułów i rozdzeielnie komponentów, podmiana linków na routerLink, dodanie Favicon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { CreateCourseComponent } from './create-course/create-course.component';
-import { CourseSiteComponent } from './course-site/course-site.component';
+import { LoginComponent } from './login-module/login/login.component';
+import { RegisterComponent } from './register-module/register/register.component';
+import { MainPageComponent } from './search-module/main-page/main-page.component';
+import { CreateCourseComponent } from './add-course-module/create-course/create-course.component';
+import { CourseSiteComponent } from './course-module/course-site/course-site.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { CourseListComponent } from './course-list/course-list.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { CreateCourseComponent } from './create-course/create-course.component';
-import { CalendarComponent } from './calendar/calendar.component';
+import { HeaderComponent } from './header-module/header/header.component';
+import { FooterComponent } from './footer-module/footer/footer.component';
+import { SidebarComponent } from './search-module/sidebar/sidebar.component';
+import { CourseListComponent } from './search-module/course-list/course-list.component';
+import { MainPageComponent } from './search-module/main-page/main-page.component';
+import { LoginComponent } from './login-module/login/login.component';
+import { RegisterComponent } from './register-module/register/register.component';
+import { CreateCourseComponent } from './add-course-module/create-course/create-course.component';
+import { CalendarComponent } from './add-course-module/calendar/calendar.component';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import { CreateCourseFormComponent } from './create-course-form/create-course-form.component';
-import { CourseSiteComponent } from './course-site/course-site.component';
+import { CreateCourseFormComponent } from './add-course-module/create-course-form/create-course-form.component';
+import { CourseSiteComponent } from './course-module/course-site/course-site.component';
 
 
 @NgModule({
