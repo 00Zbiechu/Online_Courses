c2d06b78e738155a76e8e59cb87e03f399b4a93a
Windows <00Zbiewski@gmail.com>
2023-04-23 23:53:23 +0200

    Zakończenie pracy nad wyświetlaniem kursów

--- src/app/modules/search-module/components/course-list/course-list.component.ts
+++ src/app/modules/search-module/components/course-list/course-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course, SearchServiceService } from '../../services/search-service.service';
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
   styleUrls: ['./course-list.component.scss']
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
+
+  courses: Course[];
+
+  constructor(private searchServiceService: SearchServiceService) {
+
+  }
+
+  ngOnInit(): void {
+    this.searchServiceService.getCourses().subscribe(value => {
+
+      this.courses = value;
+
+    });
+  }
 
 }
