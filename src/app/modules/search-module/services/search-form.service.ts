db1a5ba48b67acfa161baf12f9e729593b4131a3
Windows <00Zbiewski@gmail.com>
2023-05-06 02:50:28 +0200

    Filtrowanie zwracające jedną wartość - wersja próbna

---
+++ src/app/modules/search-module/services/search-form.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Course } from "../components/sidebar/model/Course";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchFormService {
+
+  private courseSource = new BehaviorSubject<Course>({ title: "", startDate: "", endDate: "", topic: "", description: "", image: "" });
+  currentCourse = this.courseSource.asObservable();
+
+  constructor() { }
+
+  changeCourse(course: Course) {
+    this.courseSource.next(course)
+  }
+
+}
