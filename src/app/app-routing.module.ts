c41fe85f1294387a0e806b64dfea98c53d700971
Windows <00Zbiewski@gmail.com>
2023-05-06 21:59:54 +0200

    Podzielenie komonentów na moduły - poza search-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateCourseComponent } from './modules/add-course-module/components/create-course/create-course.component';
+import { CourseSiteComponent } from './modules/course-module/components/course-site/course-site.component';
 import { LoginComponent } from './modules/login-module/components/login/login.component';
 import { RegisterComponent } from './modules/register-module/components/register/register.component';
 import { MainPageComponent } from './modules/search-module/components/main-page/main-page.component';
-import { CreateCourseComponent } from './modules/add-course-module/components/create-course/create-course.component';
-import { CourseSiteComponent } from './modules/course-module/components/course-site/course-site.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
--- src/app/modules/add-course-module/add-course-module.module.ts
+++ src/app/modules/add-course-module/add-course-module.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CalendarComponent } from '../add-course-module/components/calendar/calendar.component';
+import { CreateCourseFormComponent } from '../add-course-module/components/create-course-form/create-course-form.component';
+import { CreateCourseComponent } from '../add-course-module/components/create-course/create-course.component';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CalendarComponent,
+    CreateCourseComponent,
+    CreateCourseFormComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FullCalendarModule,
+    FormsModule,
+    BrowserModule
+  ],
+  exports: [
+    CalendarComponent,
+    CreateCourseComponent,
+    CreateCourseFormComponent
+  ],
 })
 export class AddCourseModuleModule { }
