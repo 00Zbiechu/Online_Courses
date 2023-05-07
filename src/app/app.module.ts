e2efea92eed1764c9352395c71de3765fc1042eb
Windows <00Zbiewski@gmail.com>
2023-05-07 17:54:20 +0200

    Podział całej aplikacji na moduły

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,32 @@
-//Dependency
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgxPaginationModule } from 'ngx-pagination';
 import { AppRoutingModule } from './app-routing.module';
-
-//Main Component
 import { AppComponent } from './app.component';
-
-//Components
-import { CourseListComponent } from './modules/search-module/components/course-list/course-list.component';
-import { MainPageComponent } from './modules/search-module/components/main-page/main-page.component';
-import { SidebarComponent } from './modules/search-module/components/sidebar/sidebar.component';
-
-//App Modules
 import { AddCourseModuleModule } from './modules/add-course-module/add-course-module.module';
 import { CourseModuleModule } from './modules/course-module/course-module.module';
 import { FooterModuleModule } from './modules/footer-module/footer-module.module';
 import { HeaderModuleModule } from './modules/header-module/header-module.module';
 import { LoginModuleModule } from './modules/login-module/login-module.module';
 import { RegisterModuleModule } from './modules/register-module/register-module.module';
+import { SearchModuleModule } from './modules/search-module/search-module.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    CourseListComponent,
-    MainPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgxPaginationModule,
-    FontAwesomeModule,
-    FormsModule,
-
     HeaderModuleModule,
     FooterModuleModule,
     LoginModuleModule,
     RegisterModuleModule,
     CourseModuleModule,
-    AddCourseModuleModule
+    AddCourseModuleModule,
+    SearchModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/search-module/search-module.module.ts
+++ src/app/modules/search-module/search-module.module.ts
@@ -1,14 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { CourseListComponent } from '../search-module/components/course-list/course-list.component';
+import { MainPageComponent } from '../search-module/components/main-page/main-page.component';
+import { SidebarComponent } from '../search-module/components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CourseListComponent,
+    MainPageComponent,
+    SidebarComponent,
+  ],
   imports: [
     BrowserModule,
-    CommonModule
-  ]
+    CommonModule,
+    NgxPaginationModule,
+    FontAwesomeModule,
+    FormsModule
+  ],
+  exports: [
+    CourseListComponent,
+    MainPageComponent,
+    SidebarComponent,
+  ],
 })
 export class SearchModuleModule { }
